7220cdc51176aa65457896994bff4061ea07e663
Otávio Ribeiro <otavio.ribeiro@gmail.com>
2019-09-05 00:10:59 -0300

    OnOffDevice

--- smartthings/smartthings-onoff.js
+++ smartthings/smartthings-onoff.js
@@ -17,6 +17,9 @@ module.exports = function(RED) {
     function SmartthingsOnOffNode(config) {
         RED.nodes.createNode(this, config);
 
+        console.log("SmartthingsOnOffNode")
+        console.log(config);
+
         this.conf = config.conf;
         this.name = config.name;
         this.device = config.device;
