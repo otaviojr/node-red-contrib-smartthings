c20d656b13c71b7b91c461d348c69a2e41ac4da9
Otávio Ribeiro <otavio.ribeiro@gmail.com>
2019-09-05 13:33:09 -0300

    working

--- smartthings/smartthings-config.js
+++ smartthings/smartthings-config.js
@@ -3,40 +3,75 @@ var SmartThings = require("smartthings-node");
 
 module.exports = function(RED) {
 
-    var token = "";
-    var callback_url = "";
-
     console.log("SmartthingsConfigNode");
 
+    var nodes = {};
+
+    function getSmartthingsApp(token) {
+
+    }
+
+    function createSmartthingsApp(token){
+
+    }
+
     function SmartthingsConfigNode(config) {
+
         RED.nodes.createNode(this, config);
+
         this.token = config.token;
         this.callbackurl = config.url;
 
-        token = this.token;
-        this.callback_url = this.callbackurl;
+        var node = this;
+
+        if(node.token && node.callbackurl){
+          token = node.token;
+          callback_url = node.callbackurl;
+
+          node.st = new SmartThings.SmartThings(token);
+
+          node.getSmartthingsApp = function() {
+            node.st.apps.getAppDetails("RedNode").then(app => {
+              console.log("Searching for Smartthings App");
+              console.log(app);
+            });
+          };
+
+          node.createSmartthingsApp = function(){
+
+          };
+
+          node.getDevices = function(type){
+            console.log("getDevices:token:"+ node.token);
+
+            node.st.devices.listDevicesByCapability(type).then(deviceList => {
+              console.log(deviceList);
+            });
+          }
+
+          nodes[node.token] = node;
+
+          node.getSmartthingsApp();
+        }
 
         console.log("SmartthingsConfigNode called");
     }
 
     RED.nodes.registerType("smartthings-config", SmartthingsConfigNode);
 
-    function getDevices(token, type){
-      console.log("getDevices:token:"+token);
-      let st = new SmartThings.SmartThings(token);
-      st.devices.listDevicesByCapability(type).then(deviceList => {
-        console.log(deviceList);
-      })
-    }
+    RED.httpAdmin.get('/smartthings/:token/devices/:type', function(req,res){
+      if(nodes[req.params.token]){
+        let node = nodes[req.params.token];
 
-    RED.httpAdmin.get('/smartthings/devices/:type', function(req,res){
-      //if (devices[req.params.id]) {
-      //  res.send(devices[req.params.id]);
-      //} else {
-        console.log("List Devices By Type: " + req.params.type)
-        getDevices(token, req.params.type);
-        res.status(404).send();
-      //}
+        console.log("List Devices By Type: " + req.params.type);
+
+        node.getDevices(req.params.type);
+      } else {
+        //TODO: 404 goes here
+      }
+
+      res.status(404).send();
+
     });
 
     RED.httpAdmin.get('/smartthings/onoff/webhook', function(req,res){
