ca4dda21f5299207cfaa5b375d2fb7e1fc831151
Otávio Ribeiro <otavio.ribeiro@gmail.com>
2019-09-08 20:31:09 -0300

    testing

--- smartthings/smartthings-config.js
+++ smartthings/smartthings-config.js
@@ -115,7 +115,7 @@ module.exports = function(RED) {
                   name: "NodeRed",
                   description: "Smartthings NodeRed Integration",
                   id: "node-red-app",
-                  permissions: ["r:devices:*"],
+                  permissions: [],
                   firstPageId: "1"
                 }
               }
@@ -145,9 +145,7 @@ module.exports = function(RED) {
                         "switch"
                       ],
                       permissions: [
-                        "r",
-                        "x",
-                        "w"
+                        "r"
                       ]
                     },
                     {
