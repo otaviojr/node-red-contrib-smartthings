9f2eec1187842fabaf553625a87b820355546bdf
Otávio Ribeiro <otavio.ribeiro@gmail.com>
2019-10-03 14:06:08 -0300

    working

--- smartthings/smartthings-color.js
+++ smartthings/smartthings-color.js
@@ -8,7 +8,7 @@ module.exports = function(RED) {
         if(s == 0) {
             r = g = b = l;
         } else {
-            const hue2rgb = function hue2rgb(p, q, t){
+            let hue2rgb = function hue2rgb(p, q, t){
                 if(t < 0) t += 1;
                 if(t > 1) t -= 1;
                 if(t < 1/6) return p + (q - p) * 6 * t;
@@ -28,13 +28,13 @@ module.exports = function(RED) {
 
     function rgbToHsl(r, g, b){
         r /= 255, g /= 255, b /= 255;
-        var max = Math.max(r, g, b), min = Math.min(r, g, b);
-        var h, s, l = (max + min) / 2;
+        const max = Math.max(r, g, b), min = Math.min(r, g, b);
+        let h, s, l = (max + min) / 2;
 
         if(max == min){
             h = s = 0;
         } else {
-            var d = max - min;
+            const d = max - min;
             s = l > 0.5 ? d / (2 - max - min) : d / (max + min);
             switch(max){
                 case r: h = (g - b) / d + (g < b ? 6 : 0); break;
@@ -168,6 +168,7 @@ module.exports = function(RED) {
                     state.temperature = status["colorTemperature"]["colorTemperature"]["value"];
                     state.temperatureUnit = status["colorTemperature"]["colorTemperature"]["unit"];
                 }
+
                 this.setState(state);
             }).catch( err => {
                 console.error("Ops... error getting device state (ColorDevice)");
@@ -243,10 +244,8 @@ module.exports = function(RED) {
                                 const state = {
                                   hue: hsl[0],
                                   saturation: hsl[1],
-                                  level: hsl[2],
-                                  color: msg.payload.value
+                                  level: hsl[2]
                                 }
-
                                 this.setState(state);
                             }).catch( (ret) => {
                                 console.error("Error updating device");
