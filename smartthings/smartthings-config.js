c839e5bba36d2415e0f4912eea77e264b5a67dd0
Otávio Ribeiro <otavio.ribeiro@gmail.com>
2019-10-04 04:02:50 -0300

    working

--- smartthings/smartthings-config.js
+++ smartthings/smartthings-config.js
@@ -30,7 +30,7 @@ module.exports = function(RED) {
                     node.st.devices.listDevicesByCapability(type).then(deviceList => {
                         console.log("Device List:");
                         console.log(deviceList);
-                        resolve(deviceList.sort( (a,b) => (a.label < b.label ? -1 : 1)));
+                        resolve(deviceList.sort( (a,b) => {return (a.label < b.label ? -1 : 1)}));
                     }).catch( err => {
                         reject(err);
                     });
