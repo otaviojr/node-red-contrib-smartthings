dd0b42a672ca3867f3f27600fcad461a8b5f4560
Otávio Ribeiro <otavio.ribeiro@gmail.com>
2019-10-04 01:27:03 -0300

    lock

---
+++ smartthings/smartthings-lock.js
@@ -0,0 +1,77 @@
+var Promise = require('promise');
+
+module.exports = function(RED) {
+
+    function SmartthingsLockNode(config) {
+        RED.nodes.createNode(this, config);
+
+        console.debug("SmartthingsLockNode")
+        console.debug(config);
+
+        this.conf = RED.nodes.getNode(config.conf);
+        this.name = config.name;
+        this.device = config.device;
+
+        this.currentStatus = 0;
+
+        this.updateStatus = function(currentStatus){
+            this.currentStatus = currentStatus;
+            let msg = {
+                topic: "lock",
+                payload: {
+                    deviceId: this.device,
+                    name: this.name,
+                    value: this.currentStatus
+                }
+            };
+            this.send(msg);
+        }
+
+        if(this.conf && this.device){
+            const callback  = (evt) => {
+                console.debug("LockDevice("+this.name+") Callback called");
+                console.debug(evt);
+                if(evt["name"] == "lock"){
+                    this.updateStatus((evt["value"].toLowerCase() === "locked" ? 1 : 0));
+                }
+            }
+
+            this.conf.registerCallback(this, this.device, callback);
+
+            this.conf.getDeviceStatus(this.device,"main/capabilities/lock").then( (status) => {
+                console.debug("LockDevice("+this.name+") Status Refreshed");
+
+                current = status["lock"]["value"];
+                if(current){
+                    this.updateStatus((current.toLowerCase() == "locked" ? 1 : 0));
+                }
+            }).catch( err => {
+                console.error("Ops... error getting device state (LockDevice)");
+                console.error(err);
+            });
+
+            this.on('input', msg => {
+                console.debug("Input Message Received");
+                if(msg && msg.payload && !isNaN(msg.payload.value) && msg.topic === "lock"){
+                    this.conf.executeDeviceCommand(this.device,[{
+                        component: "main",
+                        capability: "lock",
+                        command: (msg.payload.value == 1 ? "lock" : "unlock")
+                    }]).then( (ret) => {
+                        this.updateStatus(msg.payload.value);
+                    }).catch( (ret) => {
+                        console.error("Error updating device");
+                    });
+                }
+            });
+
+            this.on('close', () => {
+                console.debug("Closed");
+                this.conf.unregisterCallback(this, this.device, callback);
+            });
+        }
+    }
+
+    RED.nodes.registerType("smartthings-node-lock", SmartthingsLockNode);
+
+};
