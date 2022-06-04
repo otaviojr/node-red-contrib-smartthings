536b5454d28db7acefa7a3bd172f374426910f66
otaviojr <otavio.ribeiro@gmail.com>
2022-06-04 16:17:29 -0300

    webapp api

--- smartthings/smartthings-config.js
+++ smartthings/smartthings-config.js
@@ -3,6 +3,10 @@ const SmartThings = require("smartthings-node");
 const HttpSignature = require('http-signature');
 const Axios = require('axios');
 
+const SmartApp = require('@smartthings/smartapp');
+const express = require('express');
+const server = express();
+
 module.exports = function(RED) {
 
     console.log("SmartthingsConfigNode");
@@ -18,6 +22,7 @@ module.exports = function(RED) {
         console.log(config);
 
         this.token = config.token;
+        this.port = config.port;
 
         var node = this;
 
@@ -115,6 +120,43 @@ module.exports = function(RED) {
             };
 
             nodes[node.token] = node;
+
+            const smartapp = new SmartApp()
+                .enableEventLogging(2) // logs all lifecycle event requests and responses as pretty-printed JSON. Omit in production
+                .page('mainPage', (context, page, configData) => {
+                    page.section('sensors', section => {
+                        section
+                            .deviceSetting('contactSensor')
+                            .capabilities(['contactSensor'])
+                    });
+                    page.section('lights', section => {
+                        section
+                            .deviceSetting('lights')
+                            .capabilities(['switch'])
+                            .permissions('rx')
+                            .multiple(true);
+                    });
+                })
+                // Called for both INSTALLED and UPDATED lifecycle events if there is no separate installed() handler
+                .updated(async (context, updateData) => {
+                    await context.api.subscriptions.delete() // clear any existing configuration
+                    await context.api.subscriptions.subscribeToDevices(context.config.contactSensor, 'contactSensor', 'contact', 'myDeviceEventHandler');
+                })
+                .subscribedEventHandler('myDeviceEventHandler', async (context, event) => {
+                    const value = event.value === 'open' ? 'on' : 'off';
+                    await context.api.devices.sendCommands(context.config.lights, 'switch', value);
+                });
+
+            server.use(express.json());
+
+            /* Handle POST requests */
+            server.post('/', function (req, res, next) {
+              smartapp.handleHttpCallback(req, res);
+            });
+
+
+            /* Start listening at your defined PORT */
+            server.listen(node.port, () => console.log(`Server is up and running on port ${node.port}`));
         }
 
         console.log("SmartthingsConfigNode called");
