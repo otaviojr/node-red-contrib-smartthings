87ac0f35d9127dfd31835b5559b750e4701dcd2e
otaviojr <otavio.ribeiro@gmail.com>
2022-06-07 11:39:41 -0300

    SmartApp

--- smartthings/smartthings-config.js
+++ smartthings/smartthings-config.js
@@ -126,6 +126,7 @@ module.exports = function(RED) {
     const smartapp = new SmartApp()
         .enableEventLogging(2) // logs all lifecycle event requests and responses as pretty-printed JSON. Omit in production
         .page('mainPage', (context, page, configData) => {
+            page.name("NodeRed Smartthings Integration");
             page.section('accelerationSensor', section => {
                 section
                     .deviceSetting('accelerationSensor')
